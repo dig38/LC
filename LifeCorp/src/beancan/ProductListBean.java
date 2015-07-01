52cdb4c9ee8746e8a1da622d8f674f43f4a80435
Daniel Gorman <digorman38@gmail.com>
2015-07-01 16:22:55 -0400

    2

---
+++ LifeCorp/src/beancan/ProductListBean.java
@@ -0,0 +1,16 @@
+package beancan;
+
+import java.util.List;
+import model.Product;
+
+public class ProductListBean {
+	private List<Product> proList;
+
+	public List<Product> getProList() {
+		return proList;
+	}
+
+	public void setProList(List<Product> proList) {
+		this.proList = proList;
+	}
+}
--- LifeCorp/src/controller/ProductList.java
+++ LifeCorp/src/controller/ProductList.java
@@ -9,14 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Product;
 import modelcontroller.productMC;
-
 /**
- * Servlet implementation class CustomerList
+ * SERVLET implementation class CustomerList
  */
-@WebServlet("/CustomerList")
+@WebServlet("/ProductList")
 public class ProductList extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -24,21 +23,25 @@ public class ProductList extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		List<Product> proList = productMC.getAllProducts();
 		request.setAttribute("proList", proList);
-		getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+		
+		for (Product iterater : proList){
+			System.out.println(iterater.getProductName());
+		}
+		//getServletContext().getRequestDispatcher("/newOrder.jsp").forward(request, response);
 	}
 
 }
